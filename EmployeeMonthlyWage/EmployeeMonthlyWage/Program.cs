247bd9b6da481aef7de710a87a9b0c3e2356f887
Sharath <Sharus9587@gmail.com>
2021-05-04 21:05:59 +0530

    Added : calculation of Monthly Wage

--- EmployeeMonthlyWage/EmployeeMonthlyWage/Program.cs
+++ EmployeeMonthlyWage/EmployeeMonthlyWage/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
-namespace EmployeeCaseStatement
+namespace EmployeeMonthlyWage
 {
     class Program
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20; 
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-           
+
             int empHrs = 0;
             int empWage = 0;
+            int totalempWage = 0;
+            for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            { 
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            switch (empCheck)
             {
                 case IS_FULL_TIME:
                     empHrs = 8;
@@ -27,7 +31,11 @@ namespace EmployeeCaseStatement
                     break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalempWage += empWage; 
             Console.WriteLine("EmpWage : " + empWage);
         }
+            Console.WriteLine("Total Emp Wage : " + totalempWage);
     }
+}
+    
 }
